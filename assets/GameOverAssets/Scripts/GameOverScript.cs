f634656b194f8ba3772edff4e8982b849f10df55
Lywona <admin@Toms-MacBook-Pro-3.local>
2014-04-12 15:34:25 +0100

    Fixed GameOver scene

--- assets/GameOverAssets/Scripts/GameOverScript.cs
+++ assets/GameOverAssets/Scripts/GameOverScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 
+	public GUIStyle endScene;
+	public GUIStyle endMess;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,6 +18,12 @@ public class GameOverScript : MonoBehaviour {
 
     void OnGUI()
     {
-        GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 20, 250, 40), "You managed to recycle " + PlayerScore.SafeScore + " trash.");
-    }
+        GUI.Box(new Rect(Screen.width / 2 - 340, Screen.height / 2 - 20, 250, 40), "You managed to recycle " + PlayerScore.SafeScore + " trash.", endMess);
+    
+		if (GUI.Button(new Rect(Screen.width / 2 - 90, Screen.height / 2 + 60, 180, 40), "Main Menu", endScene))
+		{
+			Application.LoadLevel(0);
+		}
+
+	}
 }
