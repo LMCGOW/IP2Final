4aa05aa7840dc351e95d8bf1714eaaa3ecc60582
Lywona <admin@Toms-MacBook-Pro-3.local>
2014-04-13 17:09:11 +0100

    MainMenu polished.

--- assets/MainMenuAssets/Scripts/menuScript.cs
+++ assets/MainMenuAssets/Scripts/menuScript.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class menuScript : MonoBehaviour {
 
-	//public Texture backgroundTexture;
-	public GameObject instructionsTexture;
+	public Texture backgroundTexture;
+	public Texture instructionsTexture;
+
+	public bool instructionsTextureBool;
 
 	public int buttonWidth;
 	public int buttonHeigth;
@@ -13,7 +15,7 @@ public class menuScript : MonoBehaviour {
 	public AudioClip buttonClick;
 
 	public GUIStyle uiStyle;
-
+	public GUIStyle titleStyle;
 
 
 	// Use this for initialization
@@ -30,25 +32,36 @@ public class menuScript : MonoBehaviour {
 
 	void OnGUI () {
 
-	//GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture, ScaleMode.StretchToFill, true, 0);
+	GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture, ScaleMode.StretchToFill, true, 0);
 
-		if (instructionsTexture.activeSelf == false) { 
+		GUI.Box(new Rect(Screen.width/2 - 250, 100, 500, 150), "Operation Litter Bug", titleStyle);
+
+		if (instructionsTextureBool == false) { 
 
 			if (GUI.Button(new Rect(Screen.width/2-buttonWidth/2,Screen.height/2-buttonHeigth/2,buttonWidth,buttonHeigth), "Play", uiStyle)) {
 				Application.LoadLevel(1);
 				audio.PlayOneShot(buttonClick);
-		}
+			}
 
 			if (GUI.Button(new Rect(Screen.width/2-buttonWidth/2,(Screen.height/2-buttonHeigth/2) + buttonChangePos,buttonWidth,buttonHeigth), "Instructions", uiStyle)) {
-				instructionsTexture.SetActive(true);
+				instructionsTextureBool = true;
 				audio.PlayOneShot(buttonClick);
-		}
+			}
 		
 			if (GUI.Button(new Rect(Screen.width/2-buttonWidth/2,(Screen.height/2-buttonHeigth/2) + buttonChangePos*2,buttonWidth,buttonHeigth), "Quit", uiStyle)) {
 				Application.Quit();
 				audio.PlayOneShot(buttonClick);
-		}
+			}
 
+		}
+		else 
+		{
+			GUI.DrawTexture(new Rect(Screen.width/2 - 320, Screen.height/2 - 190, 640, 400), instructionsTexture);
+
+			if (GUI.Button(new Rect(Screen.width/2-buttonWidth/2,(Screen.height/2-buttonHeigth/2) + buttonChangePos*2,buttonWidth,buttonHeigth), "Close", uiStyle)) {
+				instructionsTextureBool = false;
+				audio.PlayOneShot(buttonClick);
+			}
 		}
 	}
 }
