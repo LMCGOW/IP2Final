a83a0a3c7490c129d45be6e8c0633d976dde66c9
Lywona <admin@Toms-MacBook-Pro-3.local>
2014-04-13 17:25:11 +0100

    Set timer to 5 minutes for reasons

--- assets/GameSceneAssets/Scripts/GameManagementScripts/Timer.cs
+++ assets/GameSceneAssets/Scripts/GameManagementScripts/Timer.cs
@@ -158,7 +158,7 @@ public class Timer : MonoBehaviour
 
 		if (!timerHandlerScript.timeSaved)
 		{
-			timerMinutes = 1.0F;
+			timerMinutes = 4.0F;
 		}
 		else {
 			timerMinutes = timerHandlerScript.timerSaveMinutes;
